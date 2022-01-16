90140bb2f83884c1685b59d7c4f710fe0c7a772d
Andy Jung <Andy.Jung@framegroup.com.au>
2022-01-16 15:53:14 +1100

    update database

---
+++ ePager.API/Migrations/20220116042053_update tables.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPIePager.Migrations
+{
+    public partial class updatetables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Visitors_Shops_ShopId",
+                table: "Visitors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Visitors_ShopId",
+                table: "Visitors");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "Recipient",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "Text",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "Messages");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedOn",
+                table: "Messages",
+                newName: "7");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderNo",
+                table: "Visitors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Visitors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mobile",
+                table: "Visitors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderNo",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Mobile",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "MessageHitories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    _5 = table.Column<DateTimeOffset>(name: "5", type: "datetimeoffset", nullable: false),
+                    Icon = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MessageId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MessageHitories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MessageHitories_Messages_MessageId",
+                        column: x => x.MessageId,
+                        principalTable: "Messages",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MessageHitories_MessageId",
+                table: "MessageHitories",
+                column: "MessageId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MessageHitories");
+
+            migrationBuilder.DropColumn(
+                name: "Mobile",
+                table: "Messages");
+
+            migrationBuilder.RenameColumn(
+                name: "7",
+                table: "Messages",
+                newName: "CreatedOn");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderNo",
+                table: "Visitors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Visitors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mobile",
+                table: "Visitors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderNo",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Recipient",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Messages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedOn",
+                table: "Messages",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Visitors_ShopId",
+                table: "Visitors",
+                column: "ShopId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Visitors_Shops_ShopId",
+                table: "Visitors",
+                column: "ShopId",
+                principalTable: "Shops",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
