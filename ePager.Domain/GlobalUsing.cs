8b189523770f98dcb841c6c6b354769f4e32ee2c
Andy Jung <Andy.Jung@framegroup.com.au>
2022-01-23 15:30:26 +1100

    Add GlobalUsing to Domain project

---
+++ ePager.Domain/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using ePager.Data.Interfaces;
+global using ePager.Data.Persistant;
+global using ePager.Data.Repositories;
+global using Microsoft.EntityFrameworkCore;
+global using ePager.Domain.Models;
+global using System.Linq.Expressions;
