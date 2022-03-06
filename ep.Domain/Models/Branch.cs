3ec5b6047e46881baf063b355e807cd672109f8e
Andy Jung <Andy.Jung@framegroup.com.au>
2022-03-06 17:56:48 +1100

    Udpate Branch

--- ep.Domain/Models/Branch.cs
+++ ep.Domain/Models/Branch.cs
@@ -11,5 +11,6 @@ namespace ep.Domain.Models
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Address { get; set; }
+        public string Mobile { get; set; }
     }
 }
