62fd3510d974f691b2df2b50d57e76e949617cd1
Andy Jung <Andy.Jung@framegroup.com.au>
2022-11-13 21:22:17 +1100

    Add Contract Project, Coordinate Model

---
+++ ep.Domain/Models/Coordinate.cs
@@ -0,0 +1,13 @@
+﻿namespace ep.Domain.Models
+{
+    public class Coordinate
+    {
+        public int Id { get; set; }
+        public int ShopId { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public DateTimeOffset? UpdatedOn { get; set; }
+        public Shop Shop { get; set; }
+    }
+}
