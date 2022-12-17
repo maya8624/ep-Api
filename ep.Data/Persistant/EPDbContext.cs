5524776d3f7e870e31df9eeab774cea293996f8f
Andy Jung <Andy.Jung@framegroup.com.au>
2022-12-17 23:05:26 +1100

    Remove Shop, Coordinate Model

--- ep.Data/Persistant/EPDbContext.cs
+++ ep.Data/Persistant/EPDbContext.cs
@@ -8,7 +8,6 @@
 
         public DbSet<Customer> Customers{ get; set; }
         public DbSet<Message> Messages { get; set; }
-        public DbSet<Shop> Shops { get; set; }
         public DbSet<Business> Business { get; set; }
     }
 }
