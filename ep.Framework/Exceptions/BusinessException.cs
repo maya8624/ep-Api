6bc079375e7c79c43571d7ce805122358377a384
Andy Jung <Andy.Jung@framegroup.com.au>
2023-02-20 08:39:12 +1100

    Add BusinessException

---
+++ ep.Framework/Exceptions/BusinessException.cs
@@ -0,0 +1,25 @@
+﻿using ep.Framework.Interfaces;
+
+namespace ep.Framework.Exceptions
+{
+    public class BusinessException : Exception, IBusinessException
+    {
+        public string? Details { get; set; }
+        public int ErrorCode { get; set; }
+
+        public BusinessException() { }
+        
+        public BusinessException(string message)
+            : base(message) { }
+        
+        public BusinessException(string message, Exception inner)
+            : base(message, inner) { }
+
+        public BusinessException(string details, int errorCode, string message)
+            : this (message)
+        {
+            Details = details;
+            ErrorCode = errorCode;
+        }
+    }
+}
---
+++ ep.Framework/Interfaces/IBusinessException.cs
@@ -0,0 +1,8 @@
+﻿namespace ep.Framework.Interfaces
+{
+    public interface IBusinessException
+    {
+        public int ErrorCode { get; set; }
+        public string? Details { get; set; }
+    }
+}
